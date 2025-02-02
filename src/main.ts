1f810b1348cd972308d49c3ef38e81f095a88143
alexisdjgoyo <alexisdjesusgoyoflores@gmail.com>
2025-02-02 16:46:26 -0400

    validaciones para crear y actualizar pokemon y manejo de excepciones

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //solo deja la data validada
+      forbidNonWhitelisted: true, //si esta y la anterior son true solo permite la data que se espera
+    }),
+  );
   app.setGlobalPrefix('api/v2');
   await app.listen(process.env.PORT ?? 3000);
 }
